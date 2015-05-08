508e63873268125bd8609f196f07ed6722b69167
chrisintriago <15cintriago@iaa.edu.ec>
2015-05-08 08:22:57 -0500

    Create Test1.py

---
+++ Test1.py
@@ -0,0 +1,13 @@
+operand1 = 95
+operand2 = 64.5
+
+print operand1, "+" , operand2, "=" , operand1 + operand2
+print operand1, "-" , operand2, "=" , operand1 - operand2
+print operand1, "*" , operand2, "=" , operand1 * operand2
+print operand1, "/" , operand2, "=" , operand1 / operand2
+print operand1, "%" , operand2, "=" , operand1 % operand2
+
+#The way I did my program is the most proficient because in this way you only have to change the signs and not the whole string.
+#I solved this problem using only the variables and the math signs, not using actual numbers. Making it easier to change
+#the program if needed in the future. 
+
