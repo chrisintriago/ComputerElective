f6b3b781e17c471a685ef1c216b6c5c69c4ccfab
chrisintriago <15cintriago@iaa.edu.ec>
2015-05-08 08:21:49 -0500

    Create HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,14 @@
+print "Hello World"
+print "Welcome to programming for absolute beginners.\nThis is Python"
+print 2015 #Printing an integer 
+print "2015" #Printing a string 
+print 3.1416 #Printing a floating point number
+
+#Expressions
+print 2 + 2
+print 2 - 2 
+print 2 * 2
+print 2 / 2
+print 3 % 2  #Modulus is remainder after division
+print 3.1416 * 2.0
+print 3.1416 * 2
