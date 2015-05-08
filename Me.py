7a2b6ec8c1d1e59f48d77b8e5223fe07b179d488
chrisintriago <15cintriago@iaa.edu.ec>
2015-05-08 08:22:19 -0500

    Create Me.py

---
+++ Me.py
@@ -0,0 +1,16 @@
+name = 'Christopher'
+lastname = 'Intriago'
+age = 18
+email = '15cintriago@iaa.edu.ec'
+address = 'Rosa Borja Ycaza 212 y Maracaibo'
+phonenumber = 593939163974
+
+print name
+print lastname
+print age
+print email
+print address
+print phonenumber
+
+
+print "My full name is," , name, lastname, ",and I am", age, "yeard old. My email is," , email, ".My address is," , address, "and my phone number is", phonenumber,
