e4dc7103b7d928dd4b42e404ee1dab305b23b044
chrisintriago <15cintriago@iaa.edu.ec>
2015-05-08 08:23:17 -0500

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,25 @@
+name = "Christopher Intriago"                                                                                    
+age = 18                                                                                                
+gpa = 4.0
+
+print name                                                                                                                  
+print age                                                                                                                   
+print gpa
+
+print 'My name is', name,', I am', age, 'years-old, and I have a', gpa, 'GPA!'
+
+print "In ten years, I wil be", age + 10
+
+#DogYears
+ageInHumanYears = 2
+ageInDogYears = ageInHumanYears * 7
+
+print 'Your dog is', ageInDogYears, 'years old!'
+print 'But my dog is', ageInHumanYears, 'yeard old in Human Years.'
+
+myNumber = 3.0 * 8 + 1
+print myNumber
+
+#Parens, Exponents, Multiplication, Division, Addition, and Subtraction
+
+print 'Hello' , 2
